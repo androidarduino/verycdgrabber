dd11423192c6bada56e010f4e5d2a3a567d31e00
vrcats <vrcats@4165cd22-f176-58d8-3538-ec973df45951>
2010-08-31 07:04:17 +0000

    main page working fine, detailed page needs some tune with the reg expr.

--- verycd.cpp
+++ verycd.cpp
@@ -1,5 +1,6 @@
 #include "verycd.h"
 
+
 VeryCDListPage::VeryCDListPage(QString f, int n)
 {
    //read the reg expr
@@ -13,6 +14,7 @@ VeryCDListPage::VeryCDListPage(QString f, int n)
     mainExpr = codec->toUnicode(file.readLine());
     mainExpr = mainExpr.left(mainExpr.length()-1);
     detailExpr = codec->toUnicode(file.readLine());
+    detailExpr = detailExpr.left(detailExpr.length()-1);
     qDebug()<<mainExpr<<detailExpr;
     file.close();
     http=new QHttp(server);
@@ -29,7 +31,7 @@ QStringList VeryCDListPage::allLinks()
 
 void VeryCDListPage::pageArrived(bool error)
 {
-    qDebug()<<"page arrived with error: "<<error<<http->error();
+    qDebug()<<"Main page arrived with error: "<<error<<http->error()<<server;
     if(error)
     {
         http->get(link);
@@ -37,12 +39,6 @@ void VeryCDListPage::pageArrived(bool error)
     }
     QTextCodec *codec = QTextCodec::codecForName("UTF-8");
     rawContent = codec->toUnicode(http->readAll());
-/*    QFile file("test.htm");
-    file.open(QIODevice::ReadOnly);
-    QTextCodec *codec1 = QTextCodec::codecForName("GB18030");
-    rawContent = codec1->toUnicode(file.readAll());
-    file.close();
-*/    //qDebug()<<rawContent<<mainExpr;
     QRegExp rx(mainExpr);
     rx.setMinimal(true);
     rx.setCaseSensitivity(Qt::CaseSensitive);
@@ -53,16 +49,99 @@ void VeryCDListPage::pageArrived(bool error)
         count++;
         lastPos=rx.indexIn(rawContent, lastPos+1 );
         QStringList captured=rx.capturedTexts();
-        //here create a detailed page object to grab details
-        captured[0]="\r\n"+QString().setNum(captured.size())+"**********************************\r\n";
-        qDebug()<<captured;
+        if(captured.join("")=="")
+            continue;
+        captured[0]=this->server;
+        VeryCDDetailPage * page=new VeryCDDetailPage(captured);
+        page->detailExpr=detailExpr;
+        detailPages<<page;
+        page->print();
+//        captured[0]="\r\n"+QString().setNum(captured.size())+"**********************************\r\n";
+//        qDebug()<<captured;
     }
     qDebug()<<"total found:"<<count;
-//    qDebug()<<rawContent;
+    delete http;
+    detailPages[0]->load();
 }
 
 void VeryCDDetailPage::pageArrived(bool error)
 {
-    error="";
-    return;
+    qDebug()<<"Detail page arrived with error: "<<error<<http->error();
+    if(error)
+    {
+        http->get(link);
+        return;
+    }
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    rawContent = codec->toUnicode(http->readAll());
+    QRegExp rx(detailExpr);
+    rx.setMinimal(true);
+    rx.setCaseSensitivity(Qt::CaseSensitive);
+    int lastPos=0;
+    int count=0;
+    while(lastPos!=-1)
+    {
+        count++;
+        lastPos=rx.indexIn(rawContent, lastPos+1 );
+        QStringList captured=rx.capturedTexts();
+        qDebug()<<captured[1]<<captured[2];
+    }
+    qDebug()<<"total found:"<<count;
+    delete http;
 }
+
+VeryCDDetailPage::VeryCDDetailPage(QStringList& p)
+{
+    int i=0;
+    server=p[i++];
+    picture=p[i++];
+    category=p[i++];
+    link=p[i++];
+    title=p[i++];
+    publisher=p[i++];
+    language=p[i++].replace("&nbsp;","");
+    year=p[i++].replace("&nbsp;","");
+    description=p[i++].trimmed();
+    created=p[i++];
+    modified=p[i++];
+    files=p[i++];
+    size=p[i++];
+    comments=p[i++];
+}
+
+void VeryCDDetailPage::load()
+{
+    qDebug()<<server<<link;
+    http=new QHttp(server);
+    connect(http, SIGNAL(done(bool)), this, SLOT(pageArrived(bool)));
+    http->get(link);
+}
+
+void VeryCDDetailPage::print()
+{
+    qDebug()<<"\r\r\r\r--------------------Detailed Page-------------\r ";
+    qDebug()<<"\rpicture:\t"<<picture;
+    qDebug()<<"\rcategory:\t"<<category;
+    qDebug()<<"\rlink:  \t\t"<<link;
+    qDebug()<<"\rtitle:  \t"<<title;
+    qDebug()<<"\rpublisher:\t"<<publisher;
+    qDebug()<<"\rlanguage:\t"<<language;
+    qDebug()<<"\ryear:  \t\t"<<year;
+    qDebug()<<"\rdescription:\t"<<description;
+    qDebug()<<"\rcreated:  \t"<<created;
+    qDebug()<<"\rmodified  \t"<<modified;
+    qDebug()<<"\rfiles  \t\t"<<files;
+    qDebug()<<"\rsize:  \t\t"<<size;
+    qDebug()<<"\rcomments: \t"<<comments;
+}
+
+
+
+
+
+
+
+
+
+
+
