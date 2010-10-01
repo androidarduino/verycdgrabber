8fef6cbd02f05e076b761161533c4821e5b648da
vrcats <vrcats@4165cd22-f176-58d8-3538-ec973df45951>
2010-10-01 05:07:49 +0000

    history class fully implemented, now to decide where to use the append function

--- cartwindow.h
+++ cartwindow.h
@@ -14,6 +14,7 @@ class CartWindow: public QDialog
         void update();
     private:
         Ui::Dialog dlg;
+        VeryCDHistory d_history;
     private slots:
         void copyToClipboard();
 };
