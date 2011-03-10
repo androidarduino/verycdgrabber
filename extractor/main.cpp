6011f147c7cfc38b8c4603e2fd8a6ae1e612aa2b
vrcats <vrcats@4165cd22-f176-58d8-3538-ec973df45951>
2011-03-10 07:37:19 +0000

    now working fully

--- extractor/main.cpp
+++ extractor/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
     QPageExtractor extractor("test.ini", "test");
+    QPageExtractorTester tester(&extractor);
     extractor.load();
     return app.exec();
 }
